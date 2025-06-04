93ad32eb9ef1e4a91685f2e4119594c601ed9317
Sreenath <sreenathnaidu2002@gmail.com>
2025-06-04 16:35:53 +0530

    added employee list table in frontend

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('upload/', views.upload_csv, name='upload_csv'),
+    path('overtime-records/', views.show_overtime_records, name='show_overtime_records'),
 ]
